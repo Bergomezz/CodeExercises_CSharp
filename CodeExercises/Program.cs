bc0734d400928bab0d865471adaf2d4f33117491
Bergomezz <bernardogomezz@yahoo.com.br>
2026-03-03 14:45:43 -0300

    primeiro commit preparando o projeto

---
+++ CodeExercises/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodingExercises
+{
+  public class Exercises
+  {
+    public static bool ContainsNegative(IEnumerable<int> numbers)
+    {
+      foreach (var number in numbers)
+      {
+        if (number < 0)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
